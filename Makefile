d77e0e1e00357b11ed11507629005221826865f8
Dmitry Kireyev <dobruiyyk@gmail.com>
2012-02-16 03:07:05 +0200

    ticket4: numerous trifles

--- Makefile
+++ Makefile
@@ -1,10 +1,17 @@
 MANAGE=django-admin.py
 
 test:
-	PYTHONPATH=`pwd` DJANGO_SETTINGS_MODULE=settings $(MANAGE) test tools personal_info
+	PYTHONPATH=`pwd` DJANGO_SETTINGS_MODULE=settings $(MANAGE) test personal_info.FilesTestCase tools.RequestMWTestCase
 
 run:
 	PYTHONPATH=`pwd` DJANGO_SETTINGS_MODULE=settings $(MANAGE) runserver
 
 syncdb:
 	PYTHONPATH=`pwd` DJANGO_SETTINGS_MODULE=settings $(MANAGE) syncdb --noinput
+
+windmill:
+	PYTHONPATH=`pwd` DJANGO_SETTINGS_MODULE=settings $(MANAGE) test personal_info
+	PYTHONPATH=`pwd` DJANGO_SETTINGS_MODULE=settings $(MANAGE) test tools
+
+shell:
+	PYTHONPATH=`pwd` DJANGO_SETTINGS_MODULE=settings $(MANAGE) shell
--- apps/tools/tests.py
+++ apps/tools/tests.py
@@ -2,11 +2,9 @@ from django.test import TestCase
 from apps.tools.models import HttpRequest
 from django.test.client import Client
 
-
 class RequestMWTestCase(TestCase):
     ''' test model that stores all http requests in the DB
     '''
-    
     def testHttpRequests(self):
         '''Http request -> +HttpRequest object
         '''
@@ -22,10 +20,31 @@ class RequestMWTestCase(TestCase):
     def testContextProcessor(self):
         '''Context processor with settings.__dict__
         '''
-        import settings
+        from django.conf import settings
         c = Client()
         response = c.get('/')
         settings_dict = settings.__dict__
         context_dict = response.context
         for i,j in settings_dict.items():
             self.assertEqual(context_dict[i], j)
+        
+    def testSettings(self):
+        '''Required params in settings.py
+        '''
+        import settings
+        self.assertEqual(True, 'TEMPLATE_CONTEXT_PROCESSORS' in settings.__dict__)
+
+from os import path
+from windmill.authoring import djangotest
+import os
+wmtests = path.join(path.dirname(path.abspath(__file__)),"windmilltests")
+for nm in os.listdir(wmtests):
+    if nm.startswith("test") and nm.endswith(".py"):
+        testnm = nm[:-3]
+        class WindmillTest(djangotest.WindmillDjangoUnitTest):
+            fixtures = [ 'http_requests_db.json']
+            test_dir = path.join(wmtests,nm)
+            browser = "firefox"
+        WindmillTest.__name__ = testnm
+        globals()[testnm] = WindmillTest
+        del WindmillTest
