ef577dbd9928b292e4b284d912281ea11f60bc75
Dmitry Kireyev <dobruiyyk@gmail.com>
2012-02-23 07:57:36 +0200

    ticket3: order_by(time)

--- apps/tools/views.py
+++ apps/tools/views.py
@@ -8,7 +8,7 @@ def requests(request):
         on a separate page show first 10 http requests that are stored by middleware
     
     '''
-    last_requests = HttpRequest.objects.all()[:10].values_list('ip', 'time',
+    last_requests = HttpRequest.objects.order_by('time')[:10].values_list('ip', 'time',
                                                                'request_method',
                                                                'request_path')
     return render_to_response('tools/requests.html',
