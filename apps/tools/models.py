5ebc0ef672ce4ef9daeefcb607a61e58c47c5064
Dmitry Kireyev <dobruiyyk@gmail.com>
2012-02-25 11:55:01 +0200

    ticket10: experimental signal

--- apps/tools/models.py
+++ apps/tools/models.py
@@ -14,3 +14,21 @@ class HttpRequest(models.Model):
     class Meta:
         verbose_name_plural = u'Http Requests'
         ordering = ['-time']
+
+from django.db.models.signals import post_save, post_delete
+import sys
+
+
+def print_object(sender, **kwargs):
+    try:
+        if kwargs['created']:
+            comment = 'created\n'
+        else:
+            comment = 'edited\n'
+    except KeyError:
+            comment = 'deleted\n'
+    sys.stderr.write('\n\nError! Model: %s, comment: %s' %
+                     (kwargs['instance'].__class__.__name__, comment))
+    sys.stderr.write('KWARGS: %s\n\n' % kwargs)
+post_save.connect(print_object, sender=None)
+post_delete.connect(print_object, sender=None)
